1a3a8f6adbb2fe2b89d49ccc450430b113626b8d
Angela Gilhotra <angelagilhotra@gmail.com>
2021-03-03 23:37:52 +0530

    fix

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ app.post('/update', async (req,res) => {
 
 app.post('/slack', async(req,res) => {
   const { user } = req.body
-  console.log(req.body)
+  console.log(req.payload)
   console.log ('received payload',user)
   let data = [
     [JSON.stringify(user)],
