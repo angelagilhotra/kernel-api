550a83e1a3098a48f58afd81e6a4268492bd17f4
Angela Gilhotra <angelagilhotra@gmail.com>
2021-05-27 19:51:00 +0530

    fix: move slack functions into one dir new: add offers workflow

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 const routes = require('express').Router()
 const gift = require('./gift')
-const slack = require('./slack')
-const juntos = require('./juntos')
-const jobs = require('./jobs')
+// const slack = require('./slack')
+const juntos = require('./slack/juntos')
+const jobs = require('./slack/jobs')
+const offers = require('./slack/offers')
 
 routes.use('/gift', gift)
 // routes.use('/slack', slack)
 routes.use('/juntos', juntos)
 routes.use('/jobs', jobs)
+routes.use('/offers', offers)
 
 routes.get('/', (req, res) => {
   res.status(200).json({ message: 'Connected!' })
