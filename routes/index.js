5f47dc1f865b3346d2942eadcb905f8d783324ca
Angela Gilhotra <angelagilhotra@gmail.com>
2021-05-28 20:28:15 +0530

    new: office hours workflow

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const routes = require('express').Router()
 const gift = require('./gift')
+const officeHoursWorkflow = require("./slack/officeHours")
 const juntos = require('./slack/juntos')
 const jobs = require('./slack/jobs')
 const offers = require('./slack/offers')
@@ -10,6 +11,7 @@ routes.use('/juntos', juntos)
 routes.use('/jobs', jobs)
 routes.use('/offers', offers)
 routes.use('/officeHours', officeHours)
+routes.use('/slack/officeHours', officeHoursWorkflow)
 
 routes.get('/', (req, res) => {
   res.status(200).json({ message: 'Connected!' })
