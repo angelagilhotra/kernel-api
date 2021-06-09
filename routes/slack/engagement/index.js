2231b3c9937e6e9f27ee601ed843e8c9fb647424
Angela Gilhotra <angelagilhotra@gmail.com>
2021-06-09 20:37:58 +0530

    new: send engagement message from any steward account

--- routes/slack/engagement/index.js
+++ routes/slack/engagement/index.js
@@ -1,23 +1,25 @@
 const routes = require('express').Router()
 const _ = require('lodash')
 const { WebClient } = require('@slack/web-api')
-const token = process.env.SLACK_USER_TOKEN
-const web = new WebClient(token)
-
-const Airtable = require("airtable");
-const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_KEY);
 
 
-/**
- * Create a new junto
- * expects event details, event id and record id
- * sends a slack message with a button to RSVP
- * updates airtable table with slack message's timestamp
- */
 routes.post('/sendMessage', async (req, res) => {
-  const data = _.pick(req.body, [
+  const data = await _.pick(req.body, [
     'message', 
-    'user_id'])
+    'user_id',
+    'steward'])
+
+  const stewardTokens = {
+    "Angela": process.env.SLACK_USER_TOKEN,
+    "Sachin": process.env.SLACK_SACHIN,
+    "John": process.env.SLACK_JOHN,
+    "Vivek": process.env.SLACK_VIVEK
+  }
+
+  const sendFromToken = stewardTokens[data.steward]
+
+  const web = new WebClient(sendFromToken)
+  let r = {"ok": false};
   try {
     r = await web.chat.postMessage({channel: data.user_id, text: data.message})
   } catch (err) {
