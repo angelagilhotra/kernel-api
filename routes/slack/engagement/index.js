0673fd59d63aa4c056c5d835cee4765b7b3b052f
Angela Gilhotra <angelagilhotra@gmail.com>
2021-06-17 15:06:13 +0530

    new: add slack engagement

--- routes/slack/engagement/index.js
+++ routes/slack/engagement/index.js
@@ -1,7 +1,48 @@
 const routes = require('express').Router()
 const _ = require('lodash')
 const { WebClient } = require('@slack/web-api')
+const Airtable = require("airtable");
+const e = require('express');
+const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_KEY);
 
+routes.post('/update', async (req, res) => {
+  // update metrics on airtable's base
+  const memberEmails = req.body["memberEmails"]
+  const juntoEmails = req.body["juntoEmails"]
+  const result = []
+  for (let r of memberEmails) {
+    let c = 0;
+    juntoEmails.forEach((e) => e == r.email? c=c+1 : c=c)
+    result.push({
+      "id": r.id,
+      "email": r.email,
+      "juntoCount": c
+    })
+  }
+  for (let r of result) {
+    if (r.juntoCount > 0) {
+      console.log ("updating for", r.email)
+      try {
+        await base("Slack Member Engagement").update([
+          {
+            "id": r.id,
+            "fields": {
+              "Juntos Created": r.juntoCount
+            }
+          }
+        ])
+      } catch (err) {
+        console.log ("error", err)
+        res.send({
+          "ok": false
+        })  
+      }
+    }
+  }
+  res.send({
+    "ok": true
+  })  
+})
 
 routes.post('/sendMessage', async (req, res) => {
   const data = await _.pick(req.body, [
@@ -13,7 +54,8 @@ routes.post('/sendMessage', async (req, res) => {
     "Angela": process.env.SLACK_USER_TOKEN,
     "Sachin": process.env.SLACK_SACHIN,
     "John": process.env.SLACK_JOHN,
-    "Vivek": process.env.SLACK_VIVEK
+    "Vivek": process.env.SLACK_VIVEK,
+    "William": process.env.SLACK_WILLIAM
   }
 
   const sendFromToken = stewardTokens[data.steward]
