d540f0f728825f733ec9503b9d8a70e8c5db08e0
Angela Gilhotra <angelagilhotra@gmail.com>
2021-06-30 14:19:15 +0530

    fix: attempt to fix: CSP

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 // import libraries
-const express = require('express')
-  , bodyParser = require('body-parser')
-  , cors = require('cors')
-const routes = require('./routes')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const routes = require('./routes');
 
 // initialise app
-const app = express()
-app.use(express.static('./gift/images'))
-app.use(express.static('./gift/static'))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json({ limit: '50mb' }))
-app.use(cors())
-app.use('/', routes)
+const app = express();
+
+app.use((req: any, res: any, next: any) => {
+  res.set({
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+      "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+      "Content-Security-Policy": "default-src *",
+      "X-Content-Security-Policy": "default-src *",
+      "X-WebKit-CSP": "default-src *"
+  })
+  next();
+});
+
+app.use(express.static('./gift/images'));
+app.use(express.static('./gift/static'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cors());
+app.use('/', routes);
 
 // set port
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`app listening at http://localhost:${port}`)
-})
+  console.log(`app listening at http://localhost:${port}`);
+});
