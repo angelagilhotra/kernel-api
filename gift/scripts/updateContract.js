3cd688c6da250e5df10e6c311331c05f71127220
Angela Gilhotra <angelagilhotra@gmail.com>
2021-07-01 21:05:21 +0530

    fix: lint

--- gift/scripts/updateContract.js
+++ gift/scripts/updateContract.js
@@ -1,37 +1,40 @@
+/* eslint-disable no-console */
 /**
  * update Root on contract
  */
 
-const ethers = require('ethers'); 
-const contract = require('./contract')
-const network = contract["active"]
-const address = contract[network]["address"]
-const rpc = contract[network]["rpc"]
-const abi = contract["abi"]
-const { tree } = require('./tree')
+const ethers = require('ethers');
+const contract = require('./contract');
 
-const pk = process.env.ADMIN_PK
+const network = contract.active;
+const { address } = contract[network];
+const { rpc } = contract[network];
+const { abi } = contract;
+const { tree } = require('./tree');
+
+const pk = process.env.ADMIN_PK;
 const customHttpProvider = new ethers.providers.JsonRpcProvider(rpc);
 
-const wallet = new ethers.Wallet(pk,customHttpProvider);
-const GiftContract = new ethers.Contract(address,abi,wallet);
+const wallet = new ethers.Wallet(pk, customHttpProvider);
+const GiftContract = new ethers.Contract(address, abi, wallet);
 
-const uri = "https://api.kernel.community/gift/raw/"
+const uri = 'https://api.kernel.community/gift/raw/';
 
 async function main() {
-  console.log ('---updating contract on',contract.active,'---')
-  console.log ('updating merkle root')
-  let root = (await tree).getRoot()
-  root = root.toString("hex")
-  let _root = "0x" + root
+  console.log('---updating contract on', contract.active, '---');
+  console.log('updating merkle root');
+  let root = (await tree).getRoot();
+  root = root.toString('hex');
+  const _root = `0x${root}`;
+  console.log('setting root:', _root);
   // update merkle root on contract
-  let r = await GiftContract.setMerkleRoot(_root)
-  console.log (r)
-  console.log ('updating token uri')
+  const r = await GiftContract.setMerkleRoot(_root);
+  console.log(r);
+  // console.log ('updating token uri')
   // update token uri link
-  let t = await GiftContract.setTokenUri(uri)
-  console.log (t)
-  console.log ('done✨')
+  // let t = await GiftContract.setTokenUri(uri)
+  // console.log (t)
+  console.log('done✨');
 }
 
-main ()
+main();
